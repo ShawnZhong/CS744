1f24e010cdc64b5ec16b5882d3701cc58f59e27f
szhong <szhong@node0.szhong-qv58271.uwmadison744-f19-pg0.wisc.cloudlab.us>
2019-09-23 23:49:01 -0500

    Update a1p3.py

--- a1p3/a1p3.py
+++ a1p3/a1p3.py
@@ -1,20 +1,21 @@
 from pyspark import SparkContext, SparkConf 
 
-input_path = "hdfs://clnodevm053-1.clemson.cloudlab.us:9000/web-BerkStan.txt"
+input_path = "/proj/uwmadison744-f19-PG0/data-part3/enwiki-pages-articles"
+# input_path = "hdfs://c220g1-030823vm-1.wisc.cloudlab.us:9000/web-BerkStan.txt"
 
 # set SparkConf and SparkContext
-conf = SparkConf().setAppName("a1p3")
+conf = SparkConf().setAppName("a1p3").setMaster("spark://c220g1-030823vm-1.wisc.cloudlab.us:7077")
 sc = SparkContext(conf=conf)
 
 # Read the file as RDD
 lines = sc.textFile(input_path)
 
 # Parse each line to a pair (from_node_id, to_node_id)
-pairs = lines.filter(lambda l: l[0] != "#")\
-             .map(lambda l: l.split())
+pairs = lines.map(lambda l: l.split("\t"))\
+             .filter(lambda l: len(l) == 2)
 
 # buckets is a list of tuples (from_id, [to_id_1, to_id_2, to_id_3, ...])
-buckets = pairs.groupByKey().cache()
+buckets = pairs.groupByKey()
 
 # Set initial rank of each page to be 1.
 # rank is a list of tuples (from_id, rank)
@@ -24,7 +25,7 @@ rank = buckets.mapValues(lambda e: 1.0)
 def compute_contribution(to_id_list, rank):
     return [(to_id, rank / len(to_id_list)) for to_id in to_id_list]
 
-for i in range(1):
+for i in range(5):
     # out_edge_ranks is a list of tuples (from_id, ([to_id_1, to_id_2, to_id_3, ...], rank_for_from_id))
     out_edge_ranks = buckets.join(rank)
 
@@ -32,9 +33,7 @@ for i in range(1):
     contrib = out_edge_ranks.flatMap(lambda e: compute_contribution(e[1][0], e[1][1]))\
                             .reduceByKey(lambda e1, e2: e1 + e2)
 
-    print(contrib.take(10))
-
     # Update each page's rank to be 0.15 + 0.85 * (sum of contributions).
     rank = contrib.mapValues(lambda l: 0.15 + 0.85 * l)
 
-rank.saveAsTextFile("output")
+print(rank.take(10))
