1c69c6ee0ee45e2f107f50cedcb68b629be44f0c
szhong <szhong@node0.szhong-qv58819.uwmadison744-f19-pg0.wisc.cloudlab.us>
2019-10-07 00:00:08 -0600

    Add MultiWorkerMirroredStrategy

--- Assignment 2/p2-LeNet/main.py
+++ Assignment 2/p2-LeNet/main.py
@@ -1,10 +1,27 @@
+import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.keras.layers import Dense, Dropout, Flatten, Activation, Input, AveragePooling2D, Conv2D
 from tensorflow.python.keras.models import Sequential
+import os
+import json
 
-batch_size = 128
+os.environ["TF_CONFIG"] = json.dumps({
+    "cluster": {
+        "worker": [
+            "node0:2223",
+            # "node1:2222",
+            # "node2:2222"
+        ]
+    },
+    "task": {
+        "index": 0,
+        "type": "worker"
+    }
+})
+
+batch_size = 100
 num_classes = 10
-epochs = 5
+epochs = 1
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -23,28 +40,41 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential([
-    Conv2D(32, kernel_size=(3, 3),
-           activation='relu',
-           input_shape=input_shape),
-    AveragePooling2D(pool_size=(2, 2), strides=(2, 2)),
-    Conv2D(16, kernel_size=(5, 5), activation='relu'),
-    AveragePooling2D(pool_size=(2, 2), strides=(2, 2)),
-    Flatten(),
-    Dense(120, activation='relu'),
-    Dense(84, activation='relu'),
-    Dense(num_classes, activation='softmax'),
-])
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+with strategy.scope():
+    model = Sequential([
+        Conv2D(32, kernel_size=(3, 3),
+               activation='relu',
+               input_shape=input_shape),
+        AveragePooling2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(16, kernel_size=(5, 5), activation='relu'),
+        AveragePooling2D(pool_size=(2, 2), strides=(2, 2)),
+        Flatten(),
+        Dense(120, activation='relu'),
+        Dense(84, activation='relu'),
+        Dense(num_classes, activation='softmax'),
+    ])
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
+
+score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
