e4d4808e21913bd4b053402f18915aef2a2401af
szhong <szhong@node0.szhong-qv58819.uwmadison744-f19-pg0.wisc.cloudlab.us>
2019-10-06 22:13:31 -0600

    update

--- Assignment 2/p1-LogisticRegression/main.py
+++ Assignment 2/p1-LogisticRegression/main.py
@@ -20,19 +20,19 @@ clusterSpec_cluster = tf.train.ClusterSpec({
         "node0:2222"
     ],
     "worker": [
-        "host_name0:2223",
-        "host_name1:2222"
+        "node0:2223",
+        "node1:2222"
     ]
 })
 
 clusterSpec_cluster2 = tf.train.ClusterSpec({
     "ps": [
-        "host_name0:2222"
+        "node0:2222"
     ],
     "worker": [
-        "host_name0:2223",
-        "host_name1:2222",
-        "host_name2:2222",
+        "node0:2223",
+        "node1:2222",
+        "node2:2222",
     ]
 })
 
@@ -43,54 +43,64 @@ clusterSpec = {
 }
 
 clusterinfo = clusterSpec[FLAGS.deploy_mode]
-server = tf.train.Server(
+server = tf.distribute.Server(
     clusterinfo,
     job_name=FLAGS.job_name,
     task_index=FLAGS.task_index
 )
 
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
 if FLAGS.job_name == "ps":
     server.join()
 elif FLAGS.job_name == "worker":
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    with tf.device(
+        tf.train.replica_device_setter(
+            worker_device="/job:worker/task:%d" % FLAGS.task_index,
+            cluster=clusterinfo
+        )
+    ):
+        x = tf.placeholder(tf.float32, [None, 784])
+        y = tf.placeholder(tf.float32, [None, 10])
+        W = tf.Variable(tf.random.uniform([784, 10]))
+        b = tf.Variable(tf.random.uniform([10]))
 
-    x = tf.placeholder(tf.float32, [None, 784])
-    y = tf.placeholder(tf.float32, [None, 10])
-    W = tf.Variable(tf.random.uniform([784, 10]))
-    b = tf.Variable(tf.random.uniform([10]))
+        pred = tf.nn.softmax(tf.matmul(x, W) + b)
+        loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred),
+                                             reduction_indices=1))
+        optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-    pred = tf.nn.softmax(tf.matmul(x, W) + b)
-    loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred),
-                                         reduction_indices=1))
-    optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+        pred_label = tf.argmax(pred, 1)
+        actual_label = tf.argmax(y, 1)
+        equality = tf.equal(pred_label, actual_label)
+        accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
 
-    pred_label = tf.argmax(pred, 1)
-    actual_label = tf.argmax(y, 1)
-    equality = tf.equal(pred_label, actual_label)
-    accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
+        tf.summary.scalar("Accuracy", accuracy)
+        tf.summary.scalar("Loss", loss)
 
-    tf.summary.scalar("Accuracy", accuracy)
-    tf.summary.scalar("Loss", loss)
-
-    metric = tf.summary.merge_all()
+        metric = tf.summary.merge_all()
 
     init = tf.global_variables_initializer()
 
-    epoch = 50000
-    batch_size = 50
-    with tf.Session() as sess:
+    epoch = 50
+    batch_size = 100
+
+    with tf.Session(server.target) as sess:
         sess.run(init)
         writer = tf.summary.FileWriter("summary", sess.graph)
 
         for i in range(epoch):
-            xs, ys = mnist.train.next_batch(batch_size)
-            sess.run(optimizer, feed_dict={x: xs, y: ys})
 
-            if i % 100 == 0:
-                xs, ys = mnist.test.next_batch(batch_size)
-                a, l, summary = sess.run(
-                    [accuracy, loss, metric], feed_dict={x: xs, y: ys})
-                print("Epoch: %d, Loss: %f, Accuracy: %f" % (i, l, a))
-                writer.add_summary(summary, i)
+            num_batch = mnist.train.num_examples // batch_size
+
+            for j in range(num_batch):
+                xs, ys = mnist.train.next_batch(batch_size)
+                sess.run(optimizer, feed_dict={x: xs, y: ys})
+
+            xs, ys = mnist.test.next_batch(batch_size)
+            a, l, summary = sess.run(
+                [accuracy, loss, metric], feed_dict={x: xs, y: ys})
+            print("Epoch: %d, Loss: %f, Accuracy: %f" % (i, l, a))
+            writer.add_summary(summary, i)
 
         writer.close()
